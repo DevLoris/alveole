ff83dea1af0df55cb00da9639288aad1bfdbe9be
Loris <devloris@gmail.com>
2019-11-13 11:16:33 +0100

    :bug: Fixes

--- led/index.js
+++ led/index.js
@@ -1,21 +1,16 @@
 const {LIST} = require('./led-list'); //use GPIO pin 4, and specify that it is output
-const io = require('socket.io');
-const express = require('express');
+const  app = require('http').createServer((req, res) => {res.write('test')});
+const io = require('socket.io')(app);
+app.listen(3000);
 require('dotenv').config();
 
-let server = express().listen(process.env.SERVER_PORT, () => {
-    let port = server.address().port;
-    console.log("\x1b[31m",'[SERVER STATUS] Server running at port ' + port, "\x1b[0m");
-
-    let socket_io = io(port);
-    socket_io.on('connection', function (client) {
-        client.on('led_on', function (id) {
-            if(LIST[id])
-                LIST[id].on();
-        });
-        client.on('led_off', function (id) {
-            if(LIST[id])
-                LIST[id].off();
-        });
-    })
+io.on('connection', function (client) {
+    client.on('led_on', function (id) {
+        if(LIST[id])
+            LIST[id].on();
+    });
+    client.on('led_off', function (id) {
+        if(LIST[id])
+            LIST[id].off();
+    });
 });
