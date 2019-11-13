e1d4f9fc9c7a5585d4c72f54d92c349bd9571d73
Loris <devloris@gmail.com>
2019-11-13 10:49:53 +0100

    :bug: Fixes

--- led/index.js
+++ led/index.js
@@ -8,7 +8,7 @@ let server = express().listen(process.env.SERVER_PORT, () => {
     console.log("\x1b[31m",'[SERVER STATUS] Server running at port ' + port, "\x1b[0m");
 
     let socket_io = io(port);
-    socket_io.io.on('connection', function (client) {
+    socket_io.on('connection', function (client) {
         client.on('led_on', function (id) {
             if(LIST[id])
                 LIST[id].on();
