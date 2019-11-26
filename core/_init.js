dac2a9da34092d1e1dfcdd009cf39a8631541ed3
Robin Simonklein <rsimonklein@gmail.com>
2019-11-26 18:09:39 +0100

    Init button

--- core/_init.js
+++ core/_init.js
@@ -14,6 +14,7 @@ const events = require("./vars/events");
 require('dotenv').config();
 
 const {JOYSTICKS} = require("../modules/gpio/joystick-list");
+const {BUTTONS} = require("../modules/gpio/button-list");
 const fileupload = require("express-fileupload");
 
 module.exports = (app) => {
