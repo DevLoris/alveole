2d99188dc6d0c1564bb70ecd4409c044058e206a
Robin Simonklein <rsimonklein@gmail.com>
2019-11-27 15:00:28 +0100

    x

--- led/button-list.js
+++ led/button-list.js
@@ -3,7 +3,7 @@ const {Button} = require('./button');
 
 const BUTTON_LIST = {
     0 : new Button(1, 26, LIST["0"]),
-    1 : new Button(2, 25, LIST["1"]),
+    1 : new Button(2, 17, LIST["1"]),
     2 : new Button(3, 4, LIST["2"]),
 };
 
