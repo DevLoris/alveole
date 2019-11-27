82b686e3cac947d450b79629a2cbdba6c067bc03
Robin Simonklein <rsimonklein@gmail.com>
2019-11-27 15:24:06 +0100

    x

--- led/socket.js
+++ led/socket.js
@@ -1,4 +1,9 @@
 var client = require("socket.io-client");
 var socket = client.connect("raspberrymain.local:8001");
 
+setTimeout(function() {
+    console.log(socket)
+}, 2000)
+
+
 module.exports.socket = socket;
