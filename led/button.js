c62a41e5ec39fda4db846d0ac00fb234e1e10361
Robin Simonklein <rsimonklein@gmail.com>
2019-11-27 14:46:30 +0100

    x

--- led/button.js
+++ led/button.js
@@ -1,16 +1,23 @@
-const Gpio = require('onoff').Gpio;
+const Gpio = require('pigpio').Gpio;
 const {socket} = require('./socket');
 
 class Button {
     constructor(internal_id, pin, led) {
-        this.gpio = new Gpio(pin, 'in', 'both');
+        this.gpio = new Gpio(pin, {mode: Gpio.INPUT, alert: true})
         this.led = led;
         this.internal_id = internal_id;
 
+        this.init()
 
-        this.gpio.watch((err, value) => {
-            if(led)
-                led.on();
+    }
+
+    init() {
+
+        // this.gpio.glitchFilter(500);
+
+        this.gpio.on('alert', (level) => {
+            if(this.led)
+                this.led.on();
             socket.emit('final-button', this.internal_id);
         });
     }
