47d4e5db9f8536dbd413633fa892205bc593c35b
Robin Simonklein <rsimonklein@gmail.com>
2019-11-27 14:27:47 +0100

    add good pins for led btn

--- led/button-list.js
+++ led/button-list.js
@@ -2,9 +2,9 @@ const {LIST} = require('./led-list');
 const {Button} = require('./button');
 
 const BUTTON_LIST = {
-    0 : new Button(1, 18, LIST["0"]),
-    1 : new Button(2, 19, LIST["1"]),
-    2 : new Button(3, 20, LIST["2"]),
+    0 : new Button(1, 26, LIST["0"]),
+    1 : new Button(2, 25, LIST["1"]),
+    2 : new Button(3, 4, LIST["2"]),
 };
 
 module.exports.BUTTON_LIST = BUTTON_LIST;
--- led/button.js
+++ led/button.js
@@ -2,8 +2,8 @@ const Gpio = require('onoff').Gpio;
 const {socket} = require('./socket');
 
 class Button {
-    constructor(internal_id, id, led) {
-        this.gpio = new Gpio(id, 'in', 'both');
+    constructor(internal_id, pin, led) {
+        this.gpio = new Gpio(pin, 'in', 'both');
         this.led = led;
         this.internal_id = internal_id;
 
