d55ad1cad3d06c2c28ccbc1ff60d364d82a58843
Loris <devloris@gmail.com>
2019-11-28 10:47:24 +0100

    x

--- led/socket.js
+++ led/socket.js
@@ -2,8 +2,11 @@ var client = require("socket.io-client");
 const {BUTTON_LIST} = require("./button-list");
 var socket = client.connect("http://raspberrymain.local:8001");
 
+console.log(BUTTON_LIST);
+
 socket.on('reset_alveoles', function () {
     console.log('RESET ALVEOLES')
+    console.log(BUTTON_LIST);
     BUTTON_LIST.forEach((button) => {
         button.reset()
     })
