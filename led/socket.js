797e838f711f56986631f2eb52cf263114f992cf
Loris <devloris@gmail.com>
2019-11-28 10:41:03 +0100

    x

--- led/socket.js
+++ led/socket.js
@@ -1,5 +1,11 @@
 var client = require("socket.io-client");
 var socket = client.connect("http://raspberrymain.local:8001");
 
+socket.on('reset_alveoles', function () {
+    console.log('RESET ALVEOLES')
+    Object.values(BUTTON_LIST).forEach((button) => {
+        button.reset()
+    })
+})
 
 module.exports.socket = socket;
