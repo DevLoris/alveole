c5c1b60d72435e68630915caf44e5a66816db90f
Loris <devloris@gmail.com>
2019-11-28 10:43:42 +0100

    x

--- led/button-list.js
+++ led/button-list.js
@@ -1,10 +1,10 @@
 const {LIST} = require('./led-list');
 const {Button} = require('./button');
 
-const BUTTON_LIST = {
-    0 : new Button(1, 26, LIST["0"]),
-    1 : new Button(2, 17, LIST["1"]),
-    2 : new Button(3, 4, LIST["2"]),
-};
+const BUTTON_LIST = [
+    new Button(1, 26, LIST["0"]),
+    new Button(2, 17, LIST["1"]),
+    new Button(3, 4, LIST["2"]),
+];
 
 module.exports.BUTTON_LIST = BUTTON_LIST;
--- led/socket.js
+++ led/socket.js
@@ -4,7 +4,7 @@ var socket = client.connect("http://raspberrymain.local:8001");
 
 socket.on('reset_alveoles', function () {
     console.log('RESET ALVEOLES')
-    Object.values(BUTTON_LIST).forEach((button) => {
+    BUTTON_LIST.forEach((button) => {
         button.reset()
     })
 })
