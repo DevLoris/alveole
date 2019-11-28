e50444611a256b022745e71376eba7150389b363
Loris <devloris@gmail.com>
2019-11-28 19:27:49 +0100

    x

--- modules/structure/lib/behavior/push-final-button.js
+++ modules/structure/lib/behavior/push-final-button.js
@@ -17,23 +17,26 @@ let pushFinalButton = (init = false) => {
     let ball_1 = SPHERO_CACHE.get(SPHERO_VARS.BALL_1);
     if(ball_1 !== undefined) {
         ball_1.allowMove = true;
+        ball_1.transfering = false;
         ball_1.state = SPHERO_VARS.STATES.NECTAR;
     }
 
     let ball_2 = SPHERO_CACHE.get(SPHERO_VARS.BALL_2);
     if(ball_2 !== undefined) {
-        ball_1.allowMove = false;
+        ball_2.allowMove = false;
         ball_2.state = SPHERO_VARS.STATES.INACTIVE;
         ball_2.spamed = false;
+        ball_2.transfering = false;
         ball_2.spamed_time = 0;
     }
 
     let ball_3 = SPHERO_CACHE.get(SPHERO_VARS.BALL_3);
     if(ball_3 !== undefined) {
-        ball_1.allowMove = false;
+        ball_3.allowMove = false;
+        ball_3.transfering = false;
         ball_3.state = SPHERO_VARS.STATES.INACTIVE;
 
-        if(init)  {
+        if(!init)  {
             SpheroJsonAnim.play('tptransparent', ball_3);
         }
     }
